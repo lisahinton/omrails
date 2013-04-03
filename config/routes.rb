4481f6fc4ca66288fdfa05ae3f829bd863194279
Lisa Hinton <lisahinton@gmail.com>
2013-04-03 10:47:23 -0700

    Temorarily remore :recoverabe module

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Omrails::Application.routes.draw do
   devise_for :users
 
-get 'about' => 'pages#about'
+   get 'about' => 'pages#about'
 
-root :to => 'pages#home'
+  root :to => 'pages#home'
+ 
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
